4c63053a1b6403a5926ceca357c2fd5801c76a79
LeAnne B. <branchleannelmb@gmail.com>
2024-08-04 21:48:40 -0400

    allows typing with custom font

--- main.cpp
+++ main.cpp
@@ -1,14 +1,33 @@
 #include <SFML/Graphics.hpp>
+#include <string>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Text Editor");
+    std::string text;
+    sf::Font font;
+    font.loadFromFile("HomemadeApple-Regular.ttf");
+
+    sf::Text textDisplay(text, font, 24);
+    textDisplay.setFillColor(sf::Color::White);
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::TextEntered) {
+                //Backspace
+                if (event.text.unicode == 8 && !text.empty()) {
+                    text.pop_back();
+                } else if (event.text.unicode < 128 && event.text.unicode != 8) {
+                    text += static_cast<char>(event.text.unicode);
+                }
+                textDisplay.setString(text);
+            }
         }
-        window.clear(sf::Color::Black);
+        //https://www.sfml-dev.org/documentation/2.0-fr/classsf_1_1Color.php
+        window.clear(sf::Color::Black); 
+        window.draw(textDisplay);
         window.display();
     }
     return 0;
