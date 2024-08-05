6a1ee95f87981df7b02cc9c0914490cb617b5942
LeAnne B. <branchleannelmb@gmail.com>
2024-08-04 21:26:47 -0400

    set up starter file with SFML

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.display();
+    }
+    return 0;
+}
+
+
+//g++ main.cpp -o test -IC:/msys64/mingw64/include -IC:/SFML/include -LC:/msys64/mingw64/lib -LC:/SFML/lib -lsfml-graphics -lsfml-window -lsfml-system
